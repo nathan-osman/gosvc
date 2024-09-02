018d65ea70451dfc7db5f8c21996952152f3d907
Nathan Osman <nathan@quickmediasolutions.com>
2024-09-02 14:49:06 -0700

    Add utility functions for integration with github.com/urfave.cli.

---
+++ cli.go
@@ -0,0 +1,53 @@
+package gosvc
+
+import (
+	"github.com/urfave/cli/v2"
+)
+
+// InstallCommand returns an "install" command suitable for use with
+// github.com/urfave/cli.
+func InstallCommand(i Installer) *cli.Command {
+	return &cli.Command{
+		Name:  "install",
+		Usage: "install the application",
+		Action: func(*cli.Context) error {
+			return i.Install()
+		},
+	}
+}
+
+// RemoveCommand returns a "remove" command suitable for use with
+// github.com/urfave/cli.
+func RemoveCommand(i Installer) *cli.Command {
+	return &cli.Command{
+		Name:  "remove",
+		Usage: "remove the application",
+		Action: func(*cli.Context) error {
+			return i.Remove()
+		},
+	}
+}
+
+// StartCommand returns a "start" command suitable for use with
+// github.com/urfave/cli.
+func StartCommand(s Starter) *cli.Command {
+	return &cli.Command{
+		Name:  "start",
+		Usage: "starts the application",
+		Action: func(*cli.Context) error {
+			return s.Start()
+		},
+	}
+}
+
+// StopCommand returns a "stop" command suitable for use with
+// github.com/urfave/cli.
+func StopCommand(s Starter) *cli.Command {
+	return &cli.Command{
+		Name:  "stop",
+		Usage: "stops the application",
+		Action: func(*cli.Context) error {
+			return s.Stop()
+		},
+	}
+}
